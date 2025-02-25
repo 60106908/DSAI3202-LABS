8ad9a444202b71267fdefaf13109878b37bade32
60106908 <60106908@udst.edu.qa>
2025-02-25 19:58:56 +0300

    Delete tests directory

--- tests/.ipynb_checkpoints/test_performance-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from src.performance import run_sequential
-
-class TestPerformance(unittest.TestCase):
-    def test_run_sequential(self):
-        run_sequential()
-
-if __name__ == "__main__":
-    unittest.main()
