f9ded249dee3d80f5ec5fdd52dbd280aa37b7a25
60106908 <60106908@udst.edu.qa>
2025-02-25 19:58:28 +0300

    Delete src directory

--- src/performance.py
+++
@@ -1,9 +0,0 @@
-import time
-from src.computation import generate_and_add_numbers, generate_and_join_letters
-
-
-def run_sequential():
-    print("Starting sequential execution")
-    generate_and_add_numbers(int(1e7))
-    generate_and_join_letters(int(1e7))
-    print("Sequential execution completed")
